9fddf6c3111b52a6d933cf7ec637bfb7b149a6e2
tsunglintsai <tsunglin.tsai@gmail.com>
2012-07-13 14:40:24 -0700

    To implement your new MVC, you must write a custom graphing View which must display the axes of the graph in addition to the plot of the program. Code will be provided on the class website which will draw axes with an origin at a given point and with a given scale, so you will not have to write the Core Graphics code for the axes, only for the graphing of the program itself. You probably will want to examine the provided axes-drawing code to understand how the scaling works before you do this or any of the following Required Tasks.

--- Calculator/CalculatorGraphicView.m
+++ Calculator/CalculatorGraphicView.m
@@ -7,18 +7,37 @@
 //
 
 #import "CalculatorGraphicView.h"
+#import "AxesDrawer.h"
+
 @interface CalculatorGraphicView()
 @property(nonatomic) CGPoint graphicOrigin;
 @property(nonatomic) CGFloat graphicScale;
-
+@property(nonatomic,strong) AxesDrawer *axesDrawer;
 @end
 @implementation CalculatorGraphicView
 @synthesize graphicOrigin = _graphicOrigin;
 @synthesize graphicScale = _graphicScale;
+@synthesize axesDrawer = _axesDrawer;
+
+- (AxesDrawer*)axesDrawer{
+    if(_axesDrawer==nil){
+        _axesDrawer = [[AxesDrawer alloc]init];
+    }
+    return _axesDrawer;
+}
+
 
 - (void)setup 
 { 
     
+}
+
+- (CGFloat) getYwithX:(int)x{
+    CGFloat result = 0.0;
+    
+    
+    
+    return result;
 }
 
 - (void)awakeFromNib 
@@ -38,7 +57,8 @@
 
 - (void)drawRect:(CGRect)rect
 {
-
+    CGPoint centerOfView = CGPointMake(self.bounds.origin.x+self.bounds.size.width/2, self.bounds.origin.y+self.bounds.size.height/2);
+    [[self.axesDrawer class]drawAxesInRect:self.bounds originAtPoint:centerOfView scale:1.0];
 }
 
 
